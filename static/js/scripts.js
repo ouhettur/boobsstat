a18465844affb01306bf655c999e89fc88106e88
Sergey Andrianov <asx926456@gmail.com>
2019-01-20 17:43:32 +0300

    add light background

---
+++ static/js/scripts.js
@@ -0,0 +1,21 @@
+function change() {
+        $("body").toggleClass('light');
+        $(".container > thead:nth-child(1) > tr:nth-child(1) > th:nth-child(1n)").toggleClass('light');
+            }
+
+$(document).ready(function () {
+    $(".timer").click(function change() {
+        $("body").toggleClass('light');
+        $(".container > thead:nth-child(1) > tr:nth-child(1) > th:nth-child(1n)").toggleClass('light');
+        if (localStorage.getItem('state') === "light") {localStorage.setItem("state","dark")}
+        else {localStorage.setItem("state","light")}
+            });
+    let state = localStorage.getItem('state');
+         if (state === null){
+            localStorage.setItem("state","dark")
+        }
+        else if (state === "light"){
+            localStorage.setItem("state","light");
+            change()
+        }
+});
