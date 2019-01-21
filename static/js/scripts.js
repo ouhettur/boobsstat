d73fef396d83067fc3b9d0d1bf2ab698d56f000a
Sergey Andrianov <asx926456@gmail.com>
2019-01-21 17:42:31 +0300

    add light background

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,22 +1,32 @@
-function change() {
+function styleSwitcher() {
     $("body").toggleClass('light');
     $(".container > thead:nth-child(1) > tr:nth-child(1) > th:nth-child(1n)").toggleClass('light');
 }
 
-$(document).ready(function () {
-    $(".timer").click(function () {
-        change();
-        if (localStorage.getItem('state') === "light") {
-            localStorage.setItem("state", "dark")
-        } else {
-            localStorage.setItem("state", "light")
-        }
-    });
-    let state = localStorage.getItem('state');
+function clickTimer() {
+    styleSwitcher();
+    if (localStorage.getItem('state') === "light") {
+        localStorage.setItem("state", "dark")
+    } else {
+        localStorage.setItem("state", "light")
+    }
+}
+
+function stateChanger(state) {
     if (state === null) {
         localStorage.setItem("state", "dark")
     } else if (state === "light") {
         localStorage.setItem("state", "light");
-        change()
+        styleSwitcher()
     }
+
+}
+
+$(document).ready(function () {
+    $(".timer").click(function () {
+        clickTimer()
+    });
+    let state = localStorage.getItem('state');
+    stateChanger(state)
+
 });
