cdf1c16318feb9de7bc3a7201a19b53ee407d967
Sergey Andrianov <asx926456@gmail.com>
2019-01-21 17:34:28 +0300

    add light background

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,21 +1,22 @@
 function change() {
-        $("body").toggleClass('light');
-        $(".container > thead:nth-child(1) > tr:nth-child(1) > th:nth-child(1n)").toggleClass('light');
-            }
+    $("body").toggleClass('light');
+    $(".container > thead:nth-child(1) > tr:nth-child(1) > th:nth-child(1n)").toggleClass('light');
+}
 
 $(document).ready(function () {
-    $(".timer").click(function change() {
-        $("body").toggleClass('light');
-        $(".container > thead:nth-child(1) > tr:nth-child(1) > th:nth-child(1n)").toggleClass('light');
-        if (localStorage.getItem('state') === "light") {localStorage.setItem("state","dark")}
-        else {localStorage.setItem("state","light")}
-            });
+    $(".timer").click(function () {
+        change();
+        if (localStorage.getItem('state') === "light") {
+            localStorage.setItem("state", "dark")
+        } else {
+            localStorage.setItem("state", "light")
+        }
+    });
     let state = localStorage.getItem('state');
-         if (state === null){
-            localStorage.setItem("state","dark")
-        }
-        else if (state === "light"){
-            localStorage.setItem("state","light");
-            change()
-        }
+    if (state === null) {
+        localStorage.setItem("state", "dark")
+    } else if (state === "light") {
+        localStorage.setItem("state", "light");
+        change()
+    }
 });
